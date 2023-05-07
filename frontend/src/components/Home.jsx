18232826592e7d436d0e966d887eb4bdb25d473b
Nicolas Felipe Vanderstichel <nfvander@gmail.com>
2023-05-07 18:28:03 -0300

    change on nav header (displays Inicio or Institucional ternary)

--- frontend/src/components/Home.jsx
+++ frontend/src/components/Home.jsx
@@ -14,13 +14,13 @@ const Home = () => {
             <div id="carousel-home" className="carousel slide" data-bs-ride="carousel">
                 <div className="carousel-inner">
                   <div className="carousel-item active">
-                    <img src="https://images.unsplash.com/photo-1668036268112-543f0ae5f148?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80" className="img-carousel" alt="imagen del club" />
+                    <img src="https://images.unsplash.com/photo-1672143015251-4f2da497e32d?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1461&q=80" className="img-carousel" alt="imagen del club" />
                   </div>
                   <div className="carousel-item">
-                    <img src="https://images.unsplash.com/photo-1482779665037-990b5b461e91?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80" className="img-carousel" alt="imagen del club" />
+                    <img src="https://images.unsplash.com/photo-1672143015251-4f2da497e32d?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1461&q=80" className="img-carousel" alt="imagen del club" />
                   </div>
                   <div className="carousel-item">
-                    <img src="https://images.unsplash.com/photo-1501879779179-4576bae71d8d?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80" className="img-carousel" alt="imagen del club" />
+                    <img src="https://images.unsplash.com/photo-1672143015251-4f2da497e32d?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1461&q=80" className="img-carousel" alt="imagen del club" />
                   </div>
                 </div>
                 <button className="carousel-control-prev" type="button" data-bs-target="#carousel-home" data-bs-slide="prev">
